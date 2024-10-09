bf208e3139a668d3110ebca9c34fbefeed934f80
armandotruglio <armando.truglio1@gmail.com>
2024-10-09 17:10:40 +0200

    Created DB connected to index.php

---
+++ db/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$movies = [
+    [
+        'title' => 'Inception',
+        "year" => 2010,
+        "genre" => "Sci-Fi",
+    ],
+    [
+        "title" => "Goodfellas",
+        "year" => 1990,
+        "genre" => "Ganfster"
+    ]
+];
